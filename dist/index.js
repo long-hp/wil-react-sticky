05c3ccad165fe04f4847daf72b975ca8460fe530
Wiloke <hpvanlong@gmail.com>
2020-06-26 17:07:38 +0700

    fix ssr closest

--- dist/index.js
+++ dist/index.js
@@ -99,22 +99,24 @@ function offset(el) {
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
 // For browsers that do not support Element.closest(), but carry support for element.matches() (or a prefixed equivalent, meaning IE9+), a polyfill exists:
-if (!Element.prototype.matches) {
-    Element.prototype.matches =
-        Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
-}
-if (!Element.prototype.closest) {
-    Element.prototype.closest = function (s) {
-        // eslint-disable-next-line @typescript-eslint/no-this-alias
-        var el = this;
-        do {
-            if (el.matches(s))
-                return el;
-            // @ts-ignore
-            el = el.parentElement || el.parentNode;
-        } while (el !== null && el.nodeType === 1);
-        return null;
-    };
+if (typeof Element !== 'undefined') {
+    if (!Element.prototype.matches) {
+        Element.prototype.matches =
+            Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+    }
+    if (!Element.prototype.closest) {
+        Element.prototype.closest = function (s) {
+            // eslint-disable-next-line @typescript-eslint/no-this-alias
+            var el = this;
+            do {
+                if (el.matches(s))
+                    return el;
+                // @ts-ignore
+                el = el.parentElement || el.parentNode;
+            } while (el !== null && el.nodeType === 1);
+            return null;
+        };
+    }
 }
 
 var Sticky = /** @class */ (function (_super) {
